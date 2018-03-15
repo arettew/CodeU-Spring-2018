b85f35cd9474a68dea377da4bf8b3faf720ad179
Andres Echeverria <andreseg@umich.edu>
2018-03-14 22:29:48 -0700

    Added an instance of UserStore to handle users
    This instance of UserStore would be in charge of storing users and checking if the username is
    already taken.

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -5,7 +5,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import codeu.model.store.basic.UserStore;
+import codeu.model.data.User;
+import java.util.UUID;
+import java.time.Instant;
 
 /**
  * Servlet class responsible for user Registration
@@ -36,5 +39,41 @@ public class RegisterServlet extends HttpServlet {
 
     response.getWriter().println("<p>Username: " + username + "</p>");
     response.getWriter().println("<p>Password: " + password + "</p>");
+
+    if (userStore.isUserRegistered(username)) {
+      request.setAttribute("error", "That username is already taken.");
+      request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+      return;
+    }
+
+    User user = new User(UUID.randomUUID(), username, password, Instant.now());
+    userStore.addUser(user);
+
+    response.sendRedirect("/login");
+  }
+
+
+  /**
+   * Store class that gives access to Users.
+   */
+
+  private UserStore userStore;
+
+  /**
+   * Set up state for handling registration-related requests. This method is only called when
+   * running in a server, not when running in a test.
+  */
+  @Override
+  public void init() throws ServletException {
+   super.init();
+   setUserStore(UserStore.getInstance());
+  }
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
   }
 }
