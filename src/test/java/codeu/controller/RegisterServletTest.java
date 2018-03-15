b76a0d74969d2696b1ca04e2a358b0a1d201511e
Andres Echeverria <andreseg@umich.edu>
2018-03-14 23:15:52 -0700

    Added tests for the RegisterServlet
    Heavily based on the LoginServlet test, I was able to write some tests for the RegisterServlet.
    I haven't checked if the tests work. Will do once the code compiles

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -1,4 +1,3 @@
-
 package codeu.controller;
 
 import java.io.IOException;
@@ -22,8 +21,8 @@ public class RegisterServletTest {
     registerServlet = new RegisterServlet();
     mockRequest = Mockito.mock(HttpServletRequest.class);
     mockResponse = Mockito.mock(HttpServletResponse.class);
-	mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-	Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/register.jsp"))
+	  mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+	  Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/register.jsp"))
 	    .thenReturn(mockRequestDispatcher);
 	}
 
@@ -33,4 +32,56 @@ public class RegisterServletTest {
 
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
 	}
+
+
+  @Test                                                                                                       
+  public void testDoPost_BadUsername() throws IOException, ServletException {                                           
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("bad !@#$% username");            
+                                                                                                          
+    RegisterServlet.doPost(mockRequest, mockResponse);                                                                     
+                                                                                                                 
+    Mockito.verify(mockRequest)                                                                                         
+    .setAttribute("error", "Please enter only letters, numbers, and spaces.");                  
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);                                           
+  }
+
+  @Test                                                                                                                 
+  public void testDoPost_NewUser() throws IOException, ServletException {                                               
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");                 
+                                                                                                                        
+    UserStore mockUserStore = Mockito.mock(UserStore.class);                                                            
+    Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(false);                
+    RegisterServlet.setUserStore(mockUserStore);                                                                           
+                                                                                                                        
+    HttpSession mockSession = Mockito.mock(HttpSession.class);                                                          
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);                                                     
+                                                                                                                        
+    loginServlet.doPost(mockRequest, mockResponse);                                                                     
+                                                                                                                        
+    ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);                  
+                                                                                                                        
+    Mockito.verify(mockUserStore).addUser(userArgumentCaptor.capture());                                                
+    Assert.assertEquals(userArgumentCaptor.getValue().getName(), "test username");                  
+                                                                                                                        
+    //Mockito.verify(mockSession).setAttribute("user", "test username");                                                  
+    Mockito.verify(mockResponse).sendRedirect("/login");                                                        
+  }
+   @Test                                                                                                                 
+   public void testDoPost_ExistingUser() throws IOException, ServletException {                                          
+     Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");                 
+                                                                                                                          
+     UserStore mockUserStore = Mockito.mock(UserStore.class);                                                            
+     Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);                 
+     RegisterServlet.setUserStore(mockUserStore);                                                                           
+                                                                                                                      
+     HttpSession mockSession = Mockito.mock(HttpSession.class);                                                          
+     Mockito.when(mockRequest.getSession()).thenReturn(mockSession);                                                     
+                                                                                                                      
+     RegisterServlet.doPost(mockRequest, mockResponse);                                                                                                                      
+	
+     Mockito.verify(mockRequest)                                                                                         
+     .setAttribute("error", "Please enter only letters, numbers, and spaces.");                                          
+     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);                                           
+   }                                                           
 }
+  
